1efd3892586bf8de606339fdffac191c7e3366fb
harpal88 <harpalsinchudasama88@gmail.com>
2025-04-09 00:25:01 +0530

    2nd

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,9 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/spl-returns-chatbot/',
+  base: './', // Changed from repo name to relative path
   plugins: [react()],
+  build: {
+    outDir: 'docs' // GitHub Pages can automatically serve from /docs
+  }
 })
